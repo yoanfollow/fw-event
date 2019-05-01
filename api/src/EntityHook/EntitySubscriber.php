f47154fb501cd0a04c26bec849497df1851fd938
jqu <jeremie.quinson@klaxoon.com>
2019-05-02 00:17:18 +0200

    Automatically add event organizer and comment autor using current user

--- api/src/EntityHook/EntitySubscriber.php
+++ api/src/EntityHook/EntitySubscriber.php
@@ -4,12 +4,28 @@
 namespace App\EntityHook;
 
 
+use App\Entity\Comment;
+use App\Entity\Event;
+use App\Entity\User;
 use App\Helpers\DateHelper;
 use Doctrine\Common\EventSubscriber;
 use Doctrine\ORM\Event\LifecycleEventArgs;
+use Symfony\Component\Security\Core\Security;
 
 class EntitySubscriber implements EventSubscriber
 {
+
+    /** @var Security $security */
+    private $security;
+
+    /**
+     * EntitySubscriber constructor.
+     * @param Security $security
+     */
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
 
     /**
      * @param LifecycleEventArgs $args
@@ -18,9 +34,25 @@ class EntitySubscriber implements EventSubscriber
     {
         $entity = $args->getObject();
 
-        // Add created at
+        // Automatically set createdAt in entity implementing AutoCreatedAtInterface (if it's not filled)
         if ($entity instanceof AutoCreatedAtInterface && empty($entity->getCreatedAt())) {
             $entity->setCreatedAt(DateHelper::getToday(DateHelper::UTC_PARIS_TZ));
+        }
+
+        // Automatically set author in comment if it's not filled
+        if ($entity instanceof Comment && empty($entity->getAuthor())) {
+            $user = $this->security->getUser();
+            if ($user instanceof User) {
+                $entity->setAuthor($user);
+            }
+        }
+
+        // Automatically set organizer in event if it's not filled
+        if ($entity instanceof Event && empty($entity->getOrganizer())) {
+            $user = $this->security->getUser();
+            if ($user instanceof User) {
+                $entity->setOrganizer($user);
+            }
         }
     }
 
@@ -31,7 +63,7 @@ class EntitySubscriber implements EventSubscriber
     {
         $entity = $args->getEntity();
 
-        // Updated at
+        // Automatically set createdAt in entity implementing AutoUpdatedAtInterface (if it's not filled)
         if ($entity instanceof AutoUpdatedAtInterface) {
             $entity->setUpdatedAt(DateHelper::getToday(DateHelper::UTC_PARIS_TZ));
         }
